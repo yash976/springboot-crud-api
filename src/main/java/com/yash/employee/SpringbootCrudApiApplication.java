442d03c87924498589092daea7bc5b8e8e47b2f8
Yash Advani <54671322+yash976@users.noreply.github.com>
2026-03-26 12:17:09 +0530

    Create SpringbootCrudApiApplication.java

---
+++ src/main/java/com/yash/employee/SpringbootCrudApiApplication.java
@@ -0,0 +1,12 @@
+package com.yash.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCrudApiApplication.class, args);
+    }
+}

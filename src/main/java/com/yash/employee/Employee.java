7ccbf1c118cb265bdd85111e822c2bc406b55335
Yash Advani <54671322+yash976@users.noreply.github.com>
2026-03-26 12:13:27 +0530

    Create Employee.java

---
+++ src/main/java/com/yash/employee/Employee.java
@@ -0,0 +1,59 @@
+package com.yash.employee;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String department;
+
+    public Employee() {}
+
+    public Employee(String name, String email, String department) {
+        this.name = name;
+        this.email = email;
+        this.department = department;
+    }
+
+    // Getters & Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
